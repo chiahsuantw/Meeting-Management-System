9e181d752ec6fa28e937c018bbd461db19021fd1
Jiaxuan Lin <mike200177@hotmail.com>
2021-12-03 16:45:42 +0800

    修改 Association Object

--- main/models.py
+++ main/models.py
@@ -1,4 +1,27 @@
+from enum import Enum
+
 from main import db
+
+
+class GenderType(Enum):
+    Male = 'Male'
+    Female = 'Female'
+
+
+class PersonType(Enum):
+    Expert = 'Expert'
+    Assistant = 'Assistant'
+    DeptProf = 'DeptProf'
+    OtherProf = 'OtherProf'
+    Student = 'Student'
+
+
+class MeetingType(Enum):
+    pass
+
+
+class MotionStatusType(Enum):
+    pass
 
 
 # minute_takers = db.Table(
@@ -23,7 +46,7 @@ class Meeting(db.Model):
     # date = db.Column(db.Date, nullable=False)
     # time = db.Column(db.Time, nullable=False)
     location = db.Column(db.String(50), nullable=False)
-    chair_speech = db.Column(db.String)  # TODO: What is the max length?
+    chair_speech = db.Column(db.Text)
 
     # Change attr. name chairman to chair
     # TODO: Meeting-Person relationship
@@ -32,6 +55,9 @@ class Meeting(db.Model):
     #                                 backref=db.backref('meetings_as_minute_taker', lazy=True))
     # attendees = db.relationship('Person', secondary=attendees, lazy='subquery',
     #                             backref=db.backref('meetings_as_attendee', lazy=True))
+
+    minute_takers = db.relationship('MinuteTaker', back_populates='meetings_as_minute_taker')
+    # attendees = db.relationship('Attendee', back_populates='meetings_as_attendee')
 
     attachments = db.relationship('Attachment', backref='meeting')
     announcements = db.relationship('Announcement', backref='meeting')
@@ -42,11 +68,11 @@ class Meeting(db.Model):
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    gender = db.Column(db.String(10))  # TODO: How to implement choose field?
+    gender = db.Column(db.Enum(GenderType))
     phone = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), unique=True)  # TODO: Can email be the primary key?
+    email = db.Column(db.String(50), unique=True)
     # Add new attr. type
-    type = db.Column(db.String(10), nullable=False)  # TODO: How to implement choose field?
+    type = db.Column(db.Enum(PersonType), nullable=False)
 
     expert_info = db.relationship('Expert', backref='basic_info', uselist=False)
     assistant_info = db.relationship('Assistant', backref='basic_info', uselist=False)
@@ -54,21 +80,24 @@ class Person(db.Model):
     other_prof_info = db.relationship('OtherProf', backref='basic_info', uselist=False)
     student_info = db.relationship('Student', backref='basic_info', uselist=False)
 
+    meetings_as_minute_taker = db.relationship('MinuteTaker', back_populates='minute_takers')
+    # meetings_as_attendee = db.relationship('Attendee', back_populates='attendees')
+
 
 class MinuteTaker(db.Model):
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'), primary_key=True)
     meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'), primary_key=True)
-    person = db.relationship('Person', back_populates='minute_takers')
-    meeting = db.relationship('Meeting', back_populates='meeting')
+    minute_takers = db.relationship('Person', back_populates='meetings_as_minute_taker')
+    meetings_as_minute_taker = db.relationship('Meeting', back_populates='minute_takers')
 
 
-class Attendee(db.Model):
-    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), primary_key=True)
-    meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'), primary_key=True)
-    is_present = db.Column(db.Boolean)
-    is_confirmed = db.Column(db.Boolean)
-    person = db.relationship('Person', back_populates='attendees')
-    meeting = db.relationship('Meeting', back_populates='meeting')
+# class Attendee(db.Model):
+#     person_id = db.Column(db.Integer, db.ForeignKey('person.id'), primary_key=True)
+#     meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'), primary_key=True)
+#     is_present = db.Column(db.Boolean)
+#     is_confirmed = db.Column(db.Boolean)
+#     person = db.relationship('Person', back_populates='attendees')
+#     meeting = db.relationship('Meeting', back_populates='meeting')
 
 
 class Expert(db.Model):
@@ -112,27 +141,27 @@ class Student(db.Model):
 class Attachment(db.Model):
     no = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(100), nullable=False)
-    filepath = db.Column(db.String, nullable=False)  # TODO: Should we set length limits?
+    filepath = db.Column(db.String, nullable=False)
     meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'))
 
 
 class Announcement(db.Model):
     no = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String, nullable=False)
+    content = db.Column(db.Text, nullable=False)
     meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'))
 
 
 class Extempore(db.Model):
     no = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String, nullable=False)
+    content = db.Column(db.Text, nullable=False)
     meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'))
 
 
 class Motion(db.Model):
     no = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String, nullable=False)
-    content = db.Column(db.String, nullable=False)
-    status = db.Column(db.String(10))  # TODO: How to implement choose field?
-    resolution = db.Column(db.String)
-    execution = db.Column(db.String)
+    description = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    status = db.Column(db.Enum(MotionStatusType))
+    resolution = db.Column(db.Text)
+    execution = db.Column(db.Text)
     meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'))
