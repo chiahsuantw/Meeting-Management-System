b4cdcc59018bb9f2f988a6af0b441d49be2df482
Jiaxuan Lin <mike200177@hotmail.com>
2021-12-22 03:26:17 +0800

    建置登入系統

--- main/views.py
+++ main/views.py
@@ -1,8 +1,30 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user
 
 from main import app
+from main.models import Person
 
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('meeting.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    email = request.form.get('email')
+    if email:
+        user = Person.query.filter_by(email=email).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('登入資訊不正確，請再試一次', 'danger')
+
+    return render_template('login.html', title='登入')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
