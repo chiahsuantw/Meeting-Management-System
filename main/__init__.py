e63ebf0a0f36f6d4fcac00b452b250da01799942
Jiaxuan Lin <mike200177@hotmail.com>
2021-12-21 18:52:10 +0800

    存檔

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,5 +7,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/meeting_
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'b4f576c819524c45fa389ced800edafb'
 db = SQLAlchemy(app)
+login = LoginManager(app)
 
 from main import views, models
