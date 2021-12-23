3d5d98d1b87fa06e0ad24630f53a80414b24d6e5
Jiaxuan Lin <mike200177@hotmail.com>
2021-12-23 19:02:51 +0800

    會議內容表單控制

--- main/static/js/new-meeting-form-control.js
+++ main/static/js/new-meeting-form-control.js
@@ -6,27 +6,62 @@ const announcementSection = $('#pAnnouncement');
 const motionSection = $('#pMotion');
 const extemporeSection = $('#pExtempore');
 
-newAnnouncementBtn.click(function () {
+newAnnouncementBtn.on('click', function () {
     // Add an announcement to the meeting
     const announcementElement = '<div class="d-flex mb-2">' +
-        '<textarea class="form-control" id="content" rows="2" ' +
+        '<textarea class="form-control" rows="2" ' +
         'aria-label="AnnouncementContent" placeholder="內容"></textarea>' +
-        '<a href="javascript:void(0)" class="delete-announcement my-auto ms-2">' +
+        '<a href="javascript:void(0)" class="my-auto ms-2">' +
         '<img src="static/images/trash-fill.svg" width="20" alt=""></a></div>';
     announcementSection.append(announcementElement);
 });
 
-newMotionBtn.click(function () {
+newMotionBtn.on('click', function () {
     // Add a motion to the meeting
-    motionSection.append();
+    const motionElement =
+        '<div class="d-flex mb-2"><div class="border rounded px-2 flex-fill">' +
+        '<div class=my-2>' +
+        '<h6>案由</h6><input class=form-control aria-label=MotionDescription>' +
+        '</div><div class=mb-2><h6>狀態</h6>' +
+        '<select class=selectpicker data-style=bg-white data-width=100% aria-label=MotionStatus>' +
+        '<option value=InDiscussion selected>討論中</option>' +
+        '<option value=InExecution>執行中</option>' +
+        '<option value=Closed>結案</option>' +
+        '</select></div>' +
+        '<div class=mb-2>' +
+        '<h6>內容</h6>' +
+        '<textarea type=text class=form-control rows=3 aria-label=MotionContent></textarea></div>' +
+        '<div class=mb-2>' +
+        '<h6>決策</h6><textarea type=text class=form-control rows=3 aria-label=MotionResolution></textarea>' +
+        '</div><div class=mb-2>' +
+        '<h6>執行</h6><textarea type=text class=form-control rows=3 aria-label=MotionExecution></textarea>' +
+        '</div></div><a href=javascript:void(0) class="my-auto ms-2">' +
+        '<img src=static/images/trash-fill.svg width=20 alt=""></a></div>';
+    motionSection.append(motionElement);
+    $('select').selectpicker();
 });
 
-newExtemporeBtn.click(function () {
+newExtemporeBtn.on('click', function () {
     // Add an extempore to the meeting
-    extemporeSection.append('<textarea class="form-control mb-2" id="content" rows="2"' +
-        ' aria-label="ExtemporeContent" placeholder="內容"></textarea>');
+    const extemporeElement = '<div class="d-flex mb-2">' +
+        '<textarea class="form-control" rows="2" ' +
+        'aria-label="ExtemporeContent" placeholder="內容"></textarea>' +
+        '<a href="javascript:void(0)" class="my-auto ms-2">' +
+        '<img src="static/images/trash-fill.svg" width="20" alt=""></a></div>';
+    extemporeSection.append(extemporeElement);
 });
 
-$('.delete-announcement').on('click', function () {
-    console.log('clicked');
+announcementSection.on('click', 'div > a', function () {
+    // Delete the announcement
+    $(this).parent().remove();
+})
+
+motionSection.on('click', 'div > a', function () {
+    // Delete the announcement
+    $(this).parent().remove();
+})
+
+extemporeSection.on('click', 'div > a', function () {
+    // Delete the announcement
+    $(this).parent().remove();
 })
