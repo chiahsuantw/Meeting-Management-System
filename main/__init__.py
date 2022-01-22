6e53a7ad509c58a7a73a6aae8e83490ba84a8989
Jiaxuan Lin <mike200177@hotmail.com>
2022-01-22 21:14:37 +0800

    HotFix 會議確認前端顯示

--- main/__init__.py
+++ main/__init__.py
@@ -16,7 +16,6 @@ app.config['UPLOAD_FOLDER'] = path.join(app.root_path, 'static', 'uploads')
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
 
 # Flask-Mail configurations
-# app.config['TESTING'] = True
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
