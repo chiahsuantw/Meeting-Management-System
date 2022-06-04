7baf0136d88f53fea88e7bc84dd83f7b1ae77f2a
Chia-Husan Lin <mike200177@hotmail.com>
2022-06-04 18:07:16 +0800

    登入前端加上密碼欄位

--- main/static/js/login-form-control.js
+++ main/static/js/login-form-control.js
@@ -4,12 +4,18 @@ $('#loginForm').validate({
         'email': {
             'required': true,
             'email': true
+        },
+        'password': {
+            'required': true
         }
     },
     'messages': {
         'email': {
             'required': '必須填寫',
             'email': '請輸入有效的電子郵件'
+        },
+        'password': {
+            'required': '必須填寫'
         }
     }
 });
